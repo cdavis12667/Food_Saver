3319fdbdd352c90a6dfb4071bfd9aea15995354e
Christian Davis <CIDavis@student.fullsail.edu>
2023-09-05 11:13:56 -0500

    Added buttons to the main menu that let you swap screens and buttons to the screens that let you go back to the menu

--- app/src/main/java/com/example/foodsaver/AddItemActivity.kt
+++ app/src/main/java/com/example/foodsaver/AddItemActivity.kt
@@ -1,11 +1,25 @@
 package com.example.foodsaver
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
 
 class AddItemActivity : ComponentActivity() {
+    //making a var for my button
+    private lateinit var addToMainButton: android.widget.Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.add_item_layout)
+        //assigning var
+        addToMainButton = findViewById(R.id.addToMainButton)
+
+        //setting to switch to main on click
+        addToMainButton.setOnClickListener(object: View.OnClickListener {
+            override fun onClick(p0: View?) {
+                val intent = Intent(this@AddItemActivity, MainActivity::class.java)
+                startActivity(intent)
+            }
+        })
     }
 }
--- app/src/main/java/com/example/foodsaver/SettingsActivity.kt
+++ app/src/main/java/com/example/foodsaver/SettingsActivity.kt
@@ -1,13 +1,27 @@
 package com.example.foodsaver
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
 
 
 class SettingsActivity : ComponentActivity() {
+    //making vars
+    private lateinit var settingToMainButton: android.widget.Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_layout)
+        //assigning vars
+        settingToMainButton = findViewById(R.id.settingToMainButton)
+        //making event to switch activity
+        settingToMainButton.setOnClickListener(object: View.OnClickListener{
+            override fun onClick(p0: View?) {
+                val intent = Intent(this@SettingsActivity, MainActivity::class.java)
+                startActivity(intent)
+            }
+
+        })
     }
 }
 
