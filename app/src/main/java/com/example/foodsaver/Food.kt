03a939a93b6c4ada71afb6bd52eaf6187832c1dd
JJLeViness <118782369+JJLeViness@users.noreply.github.com>
2023-09-17 14:55:20 -0400

    Function Added to food class to check if a date is valid.

--- app/src/main/java/com/example/foodsaver/Food.kt
+++ app/src/main/java/com/example/foodsaver/Food.kt
@@ -1,5 +1,6 @@
 package com.example.foodsaver
 
+import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.Date
 
@@ -51,11 +52,29 @@ class Food() {
 
     //function added to convert strings into Dates to avoid repetitive code
     private fun convertExpirationDate(foodItem: Food): Date {
-        val sFormat = SimpleDateFormat("MM-dd-yyy")
+        foodItem.itemExpirationDate.replace('/','-') //Added catch to convert / to -
+        val sFormat = SimpleDateFormat("MM-dd-yyyy")
 
         val formattedExpirationDate = sFormat.parse(foodItem.itemExpirationDate)
 
         return formattedExpirationDate
+    }
+
+    //Function added to check if a string is a valid date
+    fun isValidDate(Date: String):Boolean{
+        val sFormat=SimpleDateFormat("MM-dd-yyyy")
+        var isValid=false
+        //Disallows the user to enter date in any other format than the above
+        sFormat.isLenient=false
+        try{
+            val date=sFormat.parse(Date)
+            isValid=true
+        }
+        catch(e:ParseException){
+
+        }
+        return isValid
+
     }
 
 
