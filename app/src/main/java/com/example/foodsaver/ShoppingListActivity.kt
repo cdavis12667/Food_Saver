7184078961e3e2f5a2e8f69aa8d4d95ef74e4063
Clifton Myles <107996592+Zenrei02@users.noreply.github.com>
2023-09-19 00:56:37 +0900

    Shopping List functionality
    Items can be added to list manually by user. Created search dialog layout for search function. Pantry search is not yet functional. Will try another method at a later time.

--- app/src/main/java/com/example/foodsaver/ShoppingListActivity.kt
+++ app/src/main/java/com/example/foodsaver/ShoppingListActivity.kt
@@ -1,12 +1,29 @@
 package com.example.foodsaver
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.ListView
 import androidx.activity.ComponentActivity
+
 
 class ShoppingListActivity : ComponentActivity() {
     //making vars
     private lateinit var shoppingToMainButton: android.widget.Button
+    private lateinit var shopText: EditText
+    private lateinit var addButton: ImageButton
+    private lateinit var shopListView: ListView
+    private lateinit var adapter: ArrayAdapter<String> // Use ArrayAdapter for simplicity
+    private val shoppingItems = mutableListOf<String>()
+    private lateinit var foodList: List<Food>
+    private lateinit var shopSearch: ImageButton
+    private lateinit var searchDialog: AlertDialog
+    private lateinit var searchResultsListView: ListView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.shopping_list_layout)
@@ -16,6 +33,72 @@ class ShoppingListActivity : ComponentActivity() {
                 val intent = Intent(this@ShoppingListActivity, MainActivity::class.java)
                 startActivity(intent)
         }
+        shopText = findViewById(R.id.shopText)
+        addButton = findViewById(R.id.addButton)
+        shopListView = findViewById(R.id.shopListView)
+
+        shopSearch = findViewById(R.id.shopSearch)
+
+        shopSearch.setOnClickListener {
+            showSearchDialog()
+        }
+
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, shoppingItems)
+        shopListView.adapter = adapter
+
+
+        shopText.onFocusChangeListener = View.OnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                clearEditText()
+            }
+        }
+
+        addButton.setOnClickListener {
+            addItem()
+        }
+    }
+    private fun addItem() {
+        val itemName = shopText.text.toString().trim()
+        if (itemName.isNotEmpty()) {
+            shoppingItems.add(itemName)
+            adapter.notifyDataSetChanged()
+            shopText.text.clear()
+        }
+    }
+    private fun clearEditText() {
+        shopText.text.clear()
+    }
+    private fun showSearchDialog(){ //Still requires something to function as intended.
+        val dialogView = LayoutInflater.from(this).inflate(R.layout.search_dialog, null)
+        val searchText = dialogView.findViewById<EditText>(R.id.searchEditText)
+        searchResultsListView = dialogView.findViewById<ListView>(R.id.searchResultsListView)
+
+        searchDialog = AlertDialog.Builder(this)
+            .setView(dialogView)
+            .setPositiveButton("Search") { _, _ ->
+                val query = searchText.text.toString().trim()
+                searchFood(query)
+            }
+            .setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }
+            .create()
+
+        searchDialog.show()
+
+    }
+    private fun searchFood(query: String) {
+        val hardcodedData = listOf("Apple", "Banana", "Orange") // Replace with your actual food names
+        val searchResults = hardcodedData.filter { food ->
+            food.contains(query, ignoreCase = true)
+        }
+        /*val searchResults = GlobalFoodNames.filter { food ->
+            food.foodItemName.contains(query, ignoreCase = true)
+        }*/
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, searchResults)
+        searchResultsListView.adapter = adapter
+
+        adapter.notifyDataSetChanged()
     }
 }
 
