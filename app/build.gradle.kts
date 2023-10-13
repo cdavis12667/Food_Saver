8aa7b316d70a4d53bdd6ab5e04e8fee0ee475c45
Scott Parrillo <SMParrillo@student.fullsail.edu>
2023-10-12 23:40:21 -0400

    changed the home button from M to image

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,5 +69,5 @@ dependencies {
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
-    implementation("androidx.core:core-splashscreen:1.0.0")
+    implementation("androidx.core:core-splashscreen:1.0.1")
 }
